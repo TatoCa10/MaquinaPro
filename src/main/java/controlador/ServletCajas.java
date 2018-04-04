e3483e6739832dfa02e3efdfba408a08d00a2d88
Carlos Alberto <Carlos Alberto@Lenovo-PC>
2018-04-04 00:06:46 -0500

    maquina en maven (Union con david) Todo junto con Caja(admin full) Prueba 1

--- src/main/java/controlador/ServletCajas.java
+++ src/main/java/controlador/ServletCajas.java
@@ -1,6 +1,6 @@
 package controlador;
 
-
+import dao.admin.Admin_Caja;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
+import vo.Caja;
 
 /**
  *
@@ -48,6 +49,11 @@ public class ServletCajas extends HttpServlet {
         PrintWriter out = response.getWriter();
         response.setContentType("application/json");
 
+        Caja cajota = new Caja();
+        Admin_Caja cajita = new Admin_Caja();
+        int[] saldoCajita = new int[10];
+        int[] espacioCajita = new int[10];
+
         String moneda50 = request.getParameter("moneda50");
         String moneda100 = request.getParameter("moneda100");
         String moneda200 = request.getParameter("moneda200");
@@ -59,29 +65,105 @@ public class ServletCajas extends HttpServlet {
         String billete20000 = request.getParameter("billete20000");
         String billete50000 = request.getParameter("billete50000");
         String Full = "[" + moneda50 + "," + moneda100 + "," + moneda200 + "," + moneda500 + "," + moneda1000 + "," + billete2000 + "," + billete5000 + "," + billete10000 + "," + billete20000 + "," + billete50000 + "]";
-        
+
         int[] Denominaciones = new int[10];
-        Denominaciones[0] = Integer.parseInt(moneda50);
-        Denominaciones[1] = Integer.parseInt(moneda100);
-        Denominaciones[2] = Integer.parseInt(moneda200);
-        Denominaciones[3] = Integer.parseInt(moneda500);
-        Denominaciones[4] = Integer.parseInt(moneda1000);
-        Denominaciones[5] = Integer.parseInt(billete2000);
-        Denominaciones[6] = Integer.parseInt(billete5000);
-        Denominaciones[7] = Integer.parseInt(billete10000);
-        Denominaciones[8] = Integer.parseInt(billete20000);
-        Denominaciones[9] = Integer.parseInt(billete50000);
-        
-        
-        JSONObject json = new JSONObject();
-        json.put("confirmacion", Full);
-        System.out.println(Full);
-        out.print(json);
-        
-        for (int i = 0; i < Denominaciones.length; i++) {
-            System.out.println(Denominaciones[i]);
+
+        if (moneda50 == null) {
+            Denominaciones[0] = 0;
+        } else {
+            Denominaciones[0] = Integer.parseInt(moneda50);
         }
-        
+
+        if (moneda100 == null) {
+            Denominaciones[1] = 0;
+        } else {
+            Denominaciones[1] = Integer.parseInt(moneda100);
+        }
+
+        if (moneda200 == null) {
+            Denominaciones[2] = 0;
+        } else {
+            Denominaciones[2] = Integer.parseInt(moneda200);
+        }
+
+        if (moneda500 == null) {
+            Denominaciones[3] = 0;
+        } else {
+            Denominaciones[3] = Integer.parseInt(moneda500);
+        }
+
+        if (moneda1000 == null) {
+            Denominaciones[4] = 0;
+        } else {
+            Denominaciones[4] = Integer.parseInt(moneda1000);
+        }
+
+        if (billete2000 == null) {
+            Denominaciones[5] = 0;
+        } else {
+            Denominaciones[5] = Integer.parseInt(billete2000);
+        }
+
+        if (billete5000 == null) {
+            Denominaciones[6] = 0;
+        } else {
+            Denominaciones[6] = Integer.parseInt(billete5000);
+        }
+
+        if (billete10000 == null) {
+            Denominaciones[7] = 0;
+        } else {
+            Denominaciones[7] = Integer.parseInt(billete10000);
+        }
+
+        if (billete20000 == null) {
+            Denominaciones[8] = 0;
+        } else {
+            Denominaciones[8] = Integer.parseInt(billete20000);
+        }
+
+        if (billete50000 == null) {
+            Denominaciones[9] = 0;
+        } else {
+            Denominaciones[9] = Integer.parseInt(billete50000);
+        }
+
+        saldoCajita = cajita.leerCajaSaldo();
+        espacioCajita = cajita.leerCajaEspacio();
+
+        for (int i = 0; i < saldoCajita.length; i++) {
+            saldoCajita[i] = saldoCajita[i] + Denominaciones[i];
+
+            if (saldoCajita[i] > espacioCajita[i]) {
+
+                JSONObject json = new JSONObject();
+                json.put("confirmacion", "No hay espacio suficiente");
+                System.out.println("No hay espacio suficiente");
+                out.print(json);
+
+            } else {
+                cajota.setSaldo(saldoCajita);
+
+                if (cajita.modificarCaja(cajota)) {
+
+                    JSONObject json = new JSONObject();
+                    json.put("confirmacion", "Dinero agregado correctamente");
+                    System.out.println("Dinero agregado correctamente");
+                    out.print(json);
+
+                }
+            }
+        }
+
+//        JSONObject json = new JSONObject();
+//        json.put("confirmacion", Full);
+//        System.out.println(Full);
+//        out.print(json);
+//
+//        for (int i = 0; i < Denominaciones.length; i++) {
+//            System.out.println(Denominaciones[i]);
+//        }
+
     }
 
     /**
