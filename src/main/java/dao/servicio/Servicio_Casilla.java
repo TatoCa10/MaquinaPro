33373098333cf97c01a29764ee7d396baf899532
Carlos Alberto <Carlos Alberto@Lenovo-PC>
2018-04-08 20:50:59 -0500

    maquina en maven Compra prueba 5

--- src/main/java/dao/servicio/Servicio_Casilla.java
+++ src/main/java/dao/servicio/Servicio_Casilla.java
@@ -15,7 +15,7 @@ import vo.Casilla;
 
 public class Servicio_Casilla {
 
-    private Admin_Casilla casilla;
+    private Admin_Casilla casilla = new Admin_Casilla();
 
     public Boolean entregarProducto(Casilla casillas) {
 
@@ -46,7 +46,7 @@ public class Servicio_Casilla {
 
     }
 
-    public Integer consultarPrecio(Casilla casillas) {
+    public int consultarPrecio(Casilla casillas) {
 
         ArrayList<Casilla> productos = casilla.leerCasilla();
         int respuesta;
